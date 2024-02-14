4dba835dd42d5b938312b5044c67724df21304f9
sirGoulis14 <t.goulianos@gmail.com>
2024-02-14 14:46:53 +0200

    important commit

--- cinemaPlus/models.py
+++ cinemaPlus/models.py
@@ -6,9 +6,16 @@ from django.utils import timezone
 # Create your models here.
 
 class Director(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField()
+    biography = models.TextField(blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -26,6 +33,7 @@ class Movie(models.Model):
     duration = models.IntegerField()  # Duration in minutes
     release_date = models.DateField()
     imdb_url = models.URLField()
+    imdb_rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
     image = models.ImageField(null=True)
     summary = models.TextField()
     director = models.ForeignKey(Director, on_delete=models.CASCADE)
